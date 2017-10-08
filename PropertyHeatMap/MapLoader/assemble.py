54912249a84cefc023e03b1501d4d411c28697ee
Oleg <skorostnoy2000@yandex.ru>
2017-10-09 01:01:04 +0300

    MapLoader Ready

--- PropertyHeatMap/MapLoader/assemble.py
+++ PropertyHeatMap/MapLoader/assemble.py
@@ -4,28 +4,33 @@ from os.path import join
 
 map_dir = "map/"
 
-for p in os.listdir(map_dir):
-    if os.path.isdir(join(map_dir, p)):
-        pth = join(map_dir, p)
 
-        max_x, max_y = 0, 0
-        for i in os.listdir(pth):
-            l = i.split(".")
-            if l[0].isdecimal() and l[1].isdecimal():
-                max_x = max(int(l[0]), max_x)
-                max_y = max(int(l[1]), max_y)
 
-        size = [max_x+1, max_y+1]
+def assemble():
+    for p in os.listdir(map_dir):
+        if os.path.isdir(join(map_dir, p)) and p[0] == "z" and int(p[1:]) < 16:
+            pth = join(map_dir, p)
 
-        print("found max size", size)
+            max_x, max_y = 0, 0
+            for i in os.listdir(pth):
+                l = i.split(".")
+                if l[0].isdecimal() and l[1].isdecimal():
+                    max_x = max(int(l[0]), max_x)
+                    max_y = max(int(l[1]), max_y)
 
-        image_size = Image.open(join(pth, "0.0.png")).size
+            size = [max_x+1, max_y+1]
 
-        image = Image.new("RGB", (size[0]*image_size[0], size[1]*image_size[1]))
+            print("found max size", size)
 
-        for x in range(size[0]):
-            for y in range(size[1]):
-                image.paste(Image.open(join(pth, "{0}.{1}.png".format(x, y))), (x*image_size[0], y*image_size[1]))
-        image.save(join(map_dir, p+".png"))
+            image_size = Image.open(join(pth, "0.0.png")).size
 
+            image = Image.new("RGB", (size[0]*image_size[0], size[1]*image_size[1]))
+
+            for x in range(size[0]):
+                for y in range(size[1]):
+                    image.paste(Image.open(join(pth, "{0}.{1}.png".format(x, y))), (x*image_size[0], y*image_size[1]))
+            image.save(join(map_dir, p+".png"))
+
+if __name__ == "__main__":
+    assemble()
 
--- PropertyHeatMap/MapLoader/main.py
+++ PropertyHeatMap/MapLoader/main.py
@@ -4,34 +4,55 @@ import math
 
 # from 10 to 16 zoom
 UPDATE = False
-PROC_NUM = 8
+PROC_NUM = 16
 
 map_folder = "map/"
 
 zoom_start = 10
-zoom_end = 15
+zoom_end = 17
+
+
+def mercator(lat, lon, z):
+    return (2**(z-1)/math.pi)*(math.radians(lat)+math.pi),\
+           (2**(z-1)/math.pi)*(math.pi-math.log(math.tan(math.radians(lon)/2+math.pi/4)))
 
 
 def tiles_loader(ind, server, bounds, exclude):
-    url = "https://" + server + ".maps.yandex.net/tiles?l=map&v=17.10.01-0&x={}&y={}&z={}&scale=1&lang=ru_RU"
+    url = "http://" + server + ".maps.yandex.net/tiles?l=map&v=17.10.01-0&x={}&y={}&z={}&scale=1&lang=ru_RU"
+    tmp = mercator(bounds[0], bounds[1], zoom_start)
+    offset_tile_x, offset_tile_y = int(tmp[0]), int(tmp[1])
+    tmp = mercator(bounds[2], bounds[3], zoom_start)
+    tiles_x, tiles_y = int(tmp[0])-offset_tile_x, math.ceil(tmp[1])-offset_tile_y
+
     for z in range(zoom_start, zoom_end+1):
         if z not in exclude:
+            m = 2**(z-zoom_start)
+            m_tiles_x = tiles_x*m
+            m_tiles_y = tiles_y*m
+            m_offset_tile_x = offset_tile_x*m
+            m_offset_tile_y = offset_tile_y*m
+            
             zoom_folder = "z" + str(z) + "/"
-            offset_tile_x = round((180 + bounds[0]) / (360 / (2 ** z)))  # true
-            offset_tile_y = round(bounds[1] / (180 / (2 ** z)))  # - round(0.0047*2**z)
+            '''offset_tile_x = round((180 + bounds[0]) / (360 / (2 ** z)))  # true
+            offset_tile_y = round((bounds[1]) / (180 / (2 ** z)))  # - round(0.0047*2**z)
             tiles_x = round((180 + bounds[2]) / (360 / (2 ** z))) - offset_tile_x
-            tiles_y = -(round(bounds[3] / (180 / (2 ** z))) - offset_tile_y)  # - round(0.0047*2**z)
+            tiles_y = -(round((bounds[3]) / (180 / (2 ** z))) - offset_tile_y)  # - round(0.0047*2**z)'''
 
-            num_to_process = tiles_x*tiles_y//PROC_NUM + (tiles_x*tiles_y%PROC_NUM > ind)
+            num_to_process = m_tiles_x*m_tiles_y//PROC_NUM + (m_tiles_x*m_tiles_y%PROC_NUM > ind)
 
-            start_from = ind*(tiles_x*tiles_y//PROC_NUM) + min(ind, (tiles_x*tiles_y) % PROC_NUM)
+            start_from = ind*(m_tiles_x*m_tiles_y//PROC_NUM) + min(ind, (m_tiles_x*m_tiles_y) % PROC_NUM)
 
             for i in range(start_from, start_from + num_to_process):
-                x = i // tiles_y
-                y = i % tiles_y
+                x = i // m_tiles_y
+                y = i % m_tiles_y
+                while True:
+                    try:
+                        r = requests.get(url.format(m_offset_tile_x+x, m_offset_tile_y+y, z))
+                        open(map_folder + zoom_folder +"{}.{}.png".format(x, y), "wb").write(r.content)
+                        break
+                    except Exception:
+                        pass
 
-                r = requests.get(url.format(offset_tile_x+x, offset_tile_y+y, z))
-                open(map_folder + zoom_folder +"{}.{}.png".format(x, y), "wb").write(r.content)
                 # print("Process {}: done".format(ind), "{}.{}.png".format(x, y))
 
 
@@ -39,10 +60,11 @@ if __name__ == '__main__':
     import time
 
     start_time = time.clock()
-    start_x = 36.9223
-    start_y = 56.1364
-    end_x = 38.5071
-    end_y = 55.3900
+
+    start_x = 36.9141
+    start_y = 55.9737
+    end_x = 38.322
+    end_y = 55.379
     bounds = [start_x, start_y, end_x, end_y]
 
     servers = ["vec01", "vec02", "vec03", "vec04"]*(PROC_NUM//4)
@@ -62,6 +84,9 @@ if __name__ == '__main__':
     for i in processes:
         i.join()
     print("done in {}sec.".format(time.clock()-start_time))
+
+    '''import assemble
+    assemble.assemble()'''
 
 
 
