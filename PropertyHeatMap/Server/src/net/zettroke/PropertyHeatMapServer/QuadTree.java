60039bfe19d11a4e5dbb52966ca6281ebd928a0b
Oleg <skorostnoy2000@yandex.ru>
2017-10-22 00:44:21 +0300

    QuadTree addNode ready

--- PropertyHeatMap/Server/src/net/zettroke/PropertyHeatMapServer/QuadTree.java
+++ PropertyHeatMap/Server/src/net/zettroke/PropertyHeatMapServer/QuadTree.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * Created by Zettroke on 19.10.2017.
  */
 public class QuadTree {
-    static int MAX_AMOUNT = 100;
+    static int THRESHOLD = 100;
 
     class TreeNode{
         int items = 0;
@@ -25,21 +25,75 @@ public class QuadTree {
         }
 
         void split(){
+            this.isEndNode = false;
+
             int hx = (bounds[0] + bounds[2])/2;
             int hy = (bounds[1] + bounds[3])/2;
-            nw = new TreeNode(new int[]{bounds[0], bounds[1], hx, hy});
-            ne = new TreeNode(new int[]{hx, bounds[1], bounds[2], hy});
-            sw = new TreeNode(new int[]{bounds[0], hy, hx, bounds[3]});
-            se = new TreeNode(new int[]{hx, hy, bounds[2], bounds[3]});
+
+            this.nw = new TreeNode(new int[]{bounds[0], bounds[1], hx, hy});
+            this.ne = new TreeNode(new int[]{hx, bounds[1], bounds[2], hy});
+            this.sw = new TreeNode(new int[]{bounds[0], hy, hx, bounds[3]});
+            this.se = new TreeNode(new int[]{hx, hy, bounds[2], bounds[3]});
+
+            for (Node n: nodes){
+                if (n.x <= (bounds[0] + bounds[2])/2){
+                    if (n.y <= (bounds[1] + bounds[3])/2){
+                        nw.add(n);
+                    }else{
+                        ne.add(n);
+                    }
+                }else{
+                    if (n.y <= (bounds[1] + bounds[3])/2){
+                        sw.add(n);
+                    }else{
+                        se.add(n);
+                    }
+                }
+            }
+            nodes = null;
+
+
 
 
 
         }
+
+        void add(Node n){
+            if (!this.isEndNode){
+                // compare and add recursive
+                if (n.x <= (bounds[0] + bounds[2])/2){
+                    if (n.y <= (bounds[1] + bounds[3])/2){
+                        nw.add(n);
+                    }else{
+                        ne.add(n);
+                    }
+                }else{
+                    if (n.y <= (bounds[1] + bounds[3])/2){
+                        sw.add(n);
+                    }else{
+                        se.add(n);
+                    }
+                }
+            }else{
+                this.nodes.add(n);
+                this.items++;
+                if (this.items > THRESHOLD){
+                    this.split();
+                }
+            }
+        }
+
+        void add(MapShape m){
+            // Уххх, это надолго.....
+        }
+
     }
 
 
 
     TreeNode root;
+
+
 
     public QuadTree(int[] bounds){
         root = new TreeNode(bounds);
