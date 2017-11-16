4a045ea17cb22838c082a865ad8155512ecd536d
Oleg <skorostnoy2000@yandex.ru>
2017-11-16 22:59:45 +0300

    tiny structure fix

---
+++ PropertyHeatMap/Server/src/net/zettroke/PropertyHeatMapServer/handlers/ShittyHttpHandler.java
@@ -0,0 +1,8 @@
+package net.zettroke.PropertyHeatMapServer.handlers;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public interface ShittyHttpHandler {
+    public void handle(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception;
+}
